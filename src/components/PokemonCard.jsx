acf39dae712cc075957edd2c2d935b8e0025dbe3
FelipeShirae <felipeshirae@alunos.utfpr.edu.br>
2026-04-24 14:11:29 -0300

    melhorando o pokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, Chip, Box } from "@mui/material";
 
 export default function PokemonCard({ pokemon }) {
   if (!pokemon) return null;
@@ -6,20 +6,57 @@ export default function PokemonCard({ pokemon }) {
   return (
     <Card sx={{ mt: 3 }}>
       <CardContent style={{ textAlign: "center" }}>
+        
+      
         <Typography variant="h5" gutterBottom>
           {pokemon.name.toUpperCase()}
         </Typography>
 
-        <img
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name}
+       
+       <img
+        src={
+             pokemon.sprites.other?.["official-artwork"]?.front_default ||
+                pokemon.sprites.front_default
+            }
+            alt={pokemon.name}
         />
 
+      
         <Typography>Altura: {pokemon.height}</Typography>
         <Typography>Peso: {pokemon.weight}</Typography>
+
+       
+        <Box mt={2}>
+          <Typography variant="subtitle1">Tipos:</Typography>
+          {pokemon.types.map((t, index) => (
+            <Chip
+              key={index}
+              label={t.type.name}
+              color="primary"
+              sx={{ m: 0.5 }}
+            />
+          ))}
+        </Box>
+
+        {/* Habilidades */}
+        <Box mt={2}>
+          <Typography variant="subtitle1">Habilidades:</Typography>
+          {pokemon.abilities.map((a, index) => (
+            <Chip
+              key={index}
+              label={a.ability.name}
+              variant="outlined"
+              sx={{ m: 0.5 }}
+            />
+          ))}
+        </Box>
+
       </CardContent>
     </Card>
+
+
   );
+
 
   
 }
